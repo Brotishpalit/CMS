2a3a68c3f6c51e9bbdd2444d0603715362a76adb
Brotish <Brotish@DESKTOP-QKJ905I>
2016-02-23 23:27:33 +0530

    Button left

--- CMS/src/FrmAdmission2.java
+++ CMS/src/FrmAdmission2.java
@@ -13,17 +13,20 @@ public class FrmAdmission2  implements WindowListener{
 	
 	JRadioButton rdYes, rdNo;
 	
-	JLabel lblExam_pass,lblBoard_University,lblSchool_College,lblPassing_Year,lblDivision,lblPer_Mark,lblCourse_Name,
-	lblDuration,lblInstitute,lblYear,lblGrade,lbltech_Knowledge,lbllogo;
+	JLabel lblExam_pass,lblBoard_University,lblSchool_College,lblPassing_Year,
+	lblDivision,lblPer_Mark,lblCourse_Name,lblDuration,lblInstitute,lblYear,
+	lblGrade,lblParents,lbltech_Knowledge,lbllogo,lblAcadmic_Record,
+	lblFather_Name,lblOccupation,lblAnnual_Income,lblMobile;
 
-	JTextField txtExam_pass,txtBoard_University,txtSchool_College,txtPassing_Year,txtDivision,txtPer_Mark,txtCourse_Name,
-	txtDuration,txtInstitute,txtYear,txtGrade,txttech_Knowledge;
+	JTextField txtExam_pass,txtBoard_University,txtSchool_College,
+	txtPassing_Year,txtDivision,txtPer_Mark,txtCourse_Name,
+	txtDuration,txtInstitute,txtYear,txtGrade,txttech_Knowledge,
+	txtFather_Name,txtOccupation,txtAnnual_Income,txtMobile;
 
 	
 	Font f=new Font("Consolas",Font.BOLD,13);
 	Image img=new ImageIcon(this.getClass().getResource("/banner.jpg")).getImage();
-	//Image img1=new ImageIcon(this.getClass().getResource("/banner 2.jpg")).getImage();
-	
+		
 	public FrmAdmission2(){
 		
 		
@@ -39,6 +42,12 @@ public class FrmAdmission2  implements WindowListener{
 		lbllogo.setBounds(20, 5, 1050, 170);
 		lbllogo.setIcon(new ImageIcon(img));
 		frm.add(lbllogo);
+		
+		lblAcadmic_Record=new JLabel("ACCADMIC RECORD [MINIMUM QUALIFACTION: INTERMEDIATE /+2");
+		lblAcadmic_Record.setBounds(30,190,500, 30);
+		lblAcadmic_Record.setFont(f);
+		lblAcadmic_Record.setForeground(Color.DARK_GRAY);
+		frm.add(lblAcadmic_Record);
 		
 		lblExam_pass=new JLabel("EXAMINATION PASSED");
 		lblExam_pass.setBounds(30, 220, 135, 30);
@@ -153,6 +162,162 @@ public class FrmAdmission2  implements WindowListener{
 		lbltech_Knowledge.setFont(f);
 		lbltech_Knowledge.setForeground(Color.BLUE);
 		frm.add(lbltech_Knowledge);
+		
+		
+		lblCourse_Name=new JLabel("    COMPUTER COURSE NAME");
+		lblCourse_Name.setBounds(30, 360,200, 30);
+		lblCourse_Name.setFont(f);
+		lblCourse_Name.setForeground(Color.BLUE);
+		frm.add(lblCourse_Name);
+		
+		txtCourse_Name=new JTextField();
+		txtCourse_Name.setBounds(30,390,200,30);
+		txtCourse_Name.setFont(f);
+		txtCourse_Name.setForeground(Color.BLACK);
+		frm.add(txtCourse_Name);
+		
+		txtCourse_Name=new JTextField();
+		txtCourse_Name.setBounds(30,420,200,30);
+		txtCourse_Name.setFont(f);
+		txtCourse_Name.setForeground(Color.BLACK);
+		frm.add(txtCourse_Name);
+
+		lblDuration=new JLabel("      DURATION");
+		lblDuration.setBounds(230, 360,150, 30);
+		lblDuration.setFont(f);
+		lblDuration.setForeground(Color.BLUE);
+		frm.add(lblDuration);
+		
+		txtDuration=new JTextField();
+		txtDuration.setBounds(230,390,150,30);
+		txtDuration.setFont(f);
+		txtDuration.setForeground(Color.BLACK);
+		frm.add(txtDuration);
+		
+		txtDuration=new JTextField();
+		txtDuration.setBounds(230,420,150,30);
+		txtDuration.setFont(f);
+		txtDuration.setForeground(Color.BLACK);
+		frm.add(txtDuration);
+		
+		lblInstitute=new JLabel("          INSTITUTE");
+		lblInstitute.setBounds(380, 360,200, 30);
+		lblInstitute.setFont(f);
+		lblInstitute.setForeground(Color.BLUE);
+		frm.add(lblInstitute);
+		
+		txtInstitute=new JTextField();
+		txtInstitute.setBounds(380,390,200,30);
+		txtInstitute.setFont(f);
+		txtInstitute.setForeground(Color.BLACK);
+		frm.add(txtInstitute);
+		
+		txtInstitute=new JTextField();
+		txtInstitute.setBounds(380,420,200,30);
+		txtInstitute.setFont(f);
+		txtInstitute.setForeground(Color.BLACK);
+		frm.add(txtInstitute);
+		
+		lblYear=new JLabel("    Year");
+		lblYear.setBounds(580,360,120, 30);
+		lblYear.setFont(f);
+		lblYear.setForeground(Color.BLUE);
+		frm.add(lblYear);
+		
+		txtYear=new JTextField();
+		txtYear.setBounds(580,390,120,30);
+		txtYear.setFont(f);
+		txtYear.setForeground(Color.BLACK);
+		frm.add(txtYear);
+		
+		txtYear=new JTextField();
+		txtYear.setBounds(580,420,120,30);
+		txtYear.setFont(f);
+		txtYear.setForeground(Color.BLACK);
+		frm.add(txtYear);
+		
+		lblGrade=new JLabel("      GRADE");
+		lblGrade.setBounds(700,360,115, 30);
+		lblGrade.setFont(f);
+		lblGrade.setForeground(Color.BLUE);
+		frm.add(lblGrade);
+		
+		txtGrade=new JTextField();
+		txtGrade.setBounds(700,390,115,30);
+		txtGrade.setFont(f);
+		txtGrade.setForeground(Color.BLACK);
+		frm.add(txtGrade);
+		
+		txtGrade=new JTextField();
+		txtGrade.setBounds(700,420,115,30);
+		txtGrade.setFont(f);
+		txtGrade.setForeground(Color.BLACK);
+		frm.add(txtGrade);
+		
+		lblParents=new JLabel(" PARTICULAR OF THE PARENTS:");
+		lblParents.setBounds(20,460,200, 30);
+		lblParents.setFont(f);
+		lblParents.setForeground(Color.BLUE);
+		frm.add(lblParents);
+		
+		lblFather_Name=new JLabel("Father_Name");
+		lblFather_Name.setBounds(30,490,150, 30);
+		lblFather_Name.setFont(f);
+		lblFather_Name.setForeground(Color.BLUE);
+		frm.add(lblFather_Name);
+		
+		txtFather_Name=new JTextField();
+		txtFather_Name.setBounds(170,490,200,30);
+		txtFather_Name.setFont(f);
+		txtFather_Name.setForeground(Color.BLACK);
+		frm.add(txtFather_Name);
+		
+		lblOccupation=new JLabel("OCCUPATION");
+		lblOccupation.setBounds(400,490,150, 30);
+		lblOccupation.setFont(f);
+		lblOccupation.setForeground(Color.BLUE);
+		frm.add(lblOccupation);
+		
+		txtOccupation=new JTextField();
+		txtOccupation.setBounds(540,490,200,30);
+		txtOccupation.setFont(f);
+		txtOccupation.setForeground(Color.BLACK);
+		frm.add(txtOccupation);
+		
+		lblMobile=new JLabel("MOBILE No");
+		lblMobile.setBounds(400,530,150, 30);
+		lblMobile.setFont(f);
+		lblMobile.setForeground(Color.BLUE);
+		frm.add(lblMobile);
+		
+		txtMobile=new JTextField();
+		txtMobile.setBounds(540,530,200,30);
+		txtMobile.setFont(f);
+		txtMobile.setForeground(Color.BLACK);
+		frm.add(txtMobile);
+		
+		lblAnnual_Income=new JLabel("ANNUAL INCOME");
+		lblAnnual_Income.setBounds(30,530,150, 30);
+		lblAnnual_Income.setFont(f);
+		lblAnnual_Income.setForeground(Color.BLUE);
+		frm.add(lblAnnual_Income);
+		
+		txtAnnual_Income=new JTextField();
+		txtAnnual_Income.setBounds(170,530,200,30);
+		txtAnnual_Income.setFont(f);
+		txtAnnual_Income.setForeground(Color.BLACK);
+		frm.add(txtAnnual_Income);
+		
+		
+		/*txtGrade=new JTextField();
+		txtGrade.setBounds(30,490,115,30);
+		txtGrade.setFont(f);
+		txtGrade.setForeground(Color.BLACK);
+		frm.add(txtGrade);*/
+		
+
+		
+		
 		
 		rdYes=new JRadioButton("YES");
 		rdYes.setBounds(240,320, 50, 30);
